76dbd10158bcc1f3b5dfac2ea8933ff3a6421501
Kem-consolation <consolationlotachi@gmail.com>
2024-01-06 23:22:30 +0100

    refactoring the code

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
-const port = 8000;
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE_LOCAL;
+// .replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD,
+// );
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // console.log(con.connections);
+    console.log('DB successfull');
+  });
+
+// console.log(tourSchema);
+const port = process.env.port || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
